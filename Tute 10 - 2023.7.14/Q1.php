ba255b6243eedb049bd8264414d108ec851a0234
Sandani T Weerakkody <134312736+SThathsarani@users.noreply.github.com>
2023-07-14 11:46:12 +0530

    Add files via upload

---
+++ Tute 10 - 2023.7.14/Q1.php
@@ -0,0 +1,16 @@
+<?php
+    $x=6;
+    $y=4;
+
+   
+    $prod=$x*$y;
+    $quotient=$x/$y;
+
+    
+    echo "Sum:".($x+$y)."<br>";
+    echo "Difference:".($x-$y)."<br>";
+    echo "Product:".($x*$y)."<br>";
+    echo "Division:".($x/$y)."<br>";
+
+    
+    ?>
